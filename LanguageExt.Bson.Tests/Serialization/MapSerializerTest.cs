382084fa3370207d3ead015b1701bc05654444ed
Anno von Heimburg <anno.vonheimburg@40three.io>
2019-09-27 16:48:15 +0200

    Use a wrapper around the map to properly test the array serializatoin

--- LanguageExt.Bson.Tests/Serialization/MapSerializerTest.cs
+++ LanguageExt.Bson.Tests/Serialization/MapSerializerTest.cs
@@ -8,33 +8,50 @@ using static LanguageExt.Prelude;
 
 namespace LanguageExt.Bson.Serialization
 {
+    internal class WrapperDoc
+    {
+        public Map<object, object> Data { get; set; }
+    }
+    
     public class MapSerializerTest : WithBsonSerializerSetup
     {
         public static IEnumerable<object[]> TestData
         {
             get
             {
-                yield return new object[] {Map<object,object>.Empty, new BsonDocument()};
-                yield return new object[] {Map<object,object>((9, 8), (7, "bar")), new BsonDocument
+                yield return new object[] {Map<object,object>.Empty, new BsonArray()};
+                yield return new object[] {Map<object,object>((9, 8), (7, "bar")), new BsonArray 
                     {
-                        {"foo", "bar"},
-                        {"hallo", 8}
+                        new BsonDocument {
+                            {"k", 7},
+                            {"v", "bar"}
+                        },
+                        new BsonDocument {
+                            {"k", 9},
+                            {"v", 8}
+                        }
                     }
                 };
-                yield return new object[] {Map<object, object>((Guid.Empty, 109)), new BsonDocument {{"id", 109}}};
+                yield return new object[] {Map<object, object>((Guid.Empty, 109)), new BsonArray
+                {
+                    new BsonDocument{ {"k", Guid.Empty}, {"v", 109}}
+                }};
             }
         }
         
         [Theory]
         [MemberData(nameof(TestData))]
-        public void MapSerializerWorks(Map<object, object> map, BsonDocument document)
+        public void MapSerializerWorksWhenWrappedInADocument(Map<object, object> map, BsonArray array)
         {
 
-            var serialized = map.ToBsonDocument();
-            var deserialized = BsonSerializer.Deserialize<Map<object, object>>(document);
+            var wrapper = new WrapperDoc {Data = map};
+            var document = new BsonDocument {{"Data", array}};
+            
+            var serialized = wrapper.ToBsonDocument();
+            var deserialized = BsonSerializer.Deserialize<WrapperDoc>(document);
 
             serialized.Should().Equal(document);
-            deserialized.Should<(object,object)>().Equal(map);
+            deserialized.Data.Should<(object,object)>().Equal(map);
         }
         
     }
